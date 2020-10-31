3772e5b1bedb11c370a5b1d24a2b41c633185d62
AlineNecchi <alinenecchi@gmail.com>
2020-10-31 14:21:13 -0300

    Complementando exercicio

--- aula09/pessoaFisica.py
+++ aula09/pessoaFisica.py
@@ -5,14 +5,25 @@ class Fisica(Pessoa):
         Pessoa.__init__(self, nome, endereco, telefone)
         self.__cpf = cpf
         self.idade = idade
-        self.peso = peso
-        self.altura = altura
+        self.peso = float(peso)
+        self.altura = float(altura)
+        self.imc = 0
 
     def imprimir__cpf(self):
         print(self.cpf)
 
     def calculo_imc(self):
-        imc = self.peso / pow(self.altura)
-        return imc
+        self.imc = self.peso / (self.altura*self.peso)
+        return self.imc
+    
+    def imprimir_imc(self):
+        print(f"O IMC de %p é %i" % (self.nome, self.calculo_imc()))
+        
+    def imprimir_altura(self):
+        print(self.altura)
+        
+    def imprimir_peso(self):
+        print(self.peso)
+        
     
     
--- aula09/pessoaJuridica.py
+++ aula09/pessoaJuridica.py
@@ -3,6 +3,15 @@ from pessoa import Pessoa
 class Juridica(Pessoa):
     def __init__(self, nome, endereco, telefone, cnpj, inscricaoEstadual, quantidadeFuncionarios):
         Pessoa.__init__(self, nome, endereco, telefone)
-        self._cnpj = cnpj
-        self._inscricaoEstadual = inscricaoEstadual
+        self.__cnpj = cnpj
+        self.__inscricaoEstadual = inscricaoEstadual
         self.quantidadeFuncionarios = quantidadeFuncionarios
+        
+    def Imprimir_cnpj(self):
+        print(self.__cnpj)
+        
+    def Imprimir_IE(self):
+        print(self.__inscricaoEstadual)
+        
+    def Imprimir_Qtde_funcionarios(self):
+        print(self.quantidadeFuncionarios)
